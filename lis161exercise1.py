cd5e301f25be365f8a37a0b8b867e8d3c8d293c5
Asharayne <martellearro@gmail.com>
2020-03-03 15:27:35 +0800

    lis161exercise1.py

--- lis161exercise1.py
+++ lis161exercise1.py
@@ -1,4 +1,4 @@
-inp1 = input('Enter hours of work')
-inp2 = input('Enter rate per hour')
-Pay = inp1 * inp2
-print('Pay:', Pay)
+inp1 = input('Enter hours of work: ')
+inp2 = input('Enter rate per hour: ')
+pay = int(inp1) * float(inp2)
+print('Pay:', pay)
---
+++ lis161exercise2.py
@@ -0,0 +1,3 @@
+inp = input("Enter the temperature in Celsius: ")
+con = int(inp) * 9 / 5 + 32
+print("Temperature in Fahrenheit: ", con)
